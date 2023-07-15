98f25c9ba4588726f9485541eaec388c15c74598
hungeredJims <sp0943@cau.ac.kr>
2023-07-16 02:16:05 +0900

    Initial Commit

---
+++ bkj/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bkj
+//
+//  Created by 정지현 on 7/16/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
